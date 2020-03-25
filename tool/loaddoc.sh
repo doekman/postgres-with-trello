efa3f414d223314421eca745e8398857b07c35b9
Doeke Zanstra <doekman@icloud.com>
2020-03-25 17:42:09 +0100

    Added some comments to loaddoc.sh

--- tool/loaddoc.sh
+++ tool/loaddoc.sh
@@ -3,6 +3,8 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+# Idea from: <https://stackoverflow.com/a/48396608/56>
+
 if (( $# < 1 )); then
 	>&2 echo "Usage: $(basename $0) file_to_load.json"
 	exit 1
@@ -19,4 +21,5 @@ doc_column="doc"
 psql_file="${0/.sh/.psql}"
 
 # Can't use "-c" because that can't handle "psql-specific features"
+# see: <https://www.postgresql.org/docs/current/app-psql.html#APP-PSQL-INTERPOLATION>
 psql -v "table_name=$table_name" -v "doc_column=$doc_column" -v "file_to_load=$file_to_load" -f "$psql_file"
