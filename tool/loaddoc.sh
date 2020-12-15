7ff8b7ce3b51c9e3099709c123b831fa81bc1135
Doeke Zanstra <doekman@icloud.com>
2020-12-15 16:56:22 +0100

    Update loaddoc.sh

--- tool/loaddoc.sh
+++ tool/loaddoc.sh
@@ -23,4 +23,4 @@ psql_file="${0/.sh/.psql}"
 
 # Can't use "-c" because that can't handle "psql-specific features"
 # see: <https://www.postgresql.org/docs/current/app-psql.html#APP-PSQL-INTERPOLATION>
-psql -v "table_schema=$table_schema" -v "table_name=$table_name" -v "doc_column=$doc_column" -v "file_to_load=$file_to_load" -f "$psql_file"|sed 's/^INSERT /UPSERT /'
+psql --no-psqlrc -v "table_schema=$table_schema" -v "table_name=$table_name" -v "doc_column=$doc_column" -v "file_to_load=$file_to_load" -f "$psql_file"|sed 's/^INSERT /UPSERT /'
