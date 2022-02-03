f1552666dc4dd0c424659406c610a6a0d3fc49de
Doeke Zanstra <doekman@icloud.com>
2022-02-03 15:44:42 +0100

    Enhanced output of tool/loaddoc.sh

--- tool/loaddoc.sh
+++ tool/loaddoc.sh
@@ -5,8 +5,21 @@ IFS=$'\n\t'
 
 # Idea from: <https://stackoverflow.com/a/48396608/56>
 
+table_schema="trello"
+table_name="document"
+doc_column="doc"
+
 if (( $# < 1 )); then
-	>&2 echo "Usage: $(basename $0) file_to_load.json [another/file.json...]"
+	>&2 echo "Usage: $(basename "$0") file_to_load.json [another/file.json...]
+
+Load one or more JSON documents in to the current (env PG*) database. Table info:
+     table schema: ${table_schema}
+       table name: ${table_name}
+        id column: auto generated (int)
+    id_doc column: filled by trigger from ${doc_column}->>'id' json value.
+       ${doc_column} column: the json-document
+"
+	
 	exit 1
 fi
 
@@ -17,14 +30,14 @@ while (( $# >= 1 )); do
 		exit 1
 	fi
 
-	table_schema="trello"
-	table_name="document"
-	doc_column="doc"
 	psql_file="${0/.sh/.psql}"
 
+	s=$( printf "%$(tput cols)s" )
+	echo "${s// /-}"
+	echo "json file | $file_to_load "
 	# Can't use "-c" because that can't handle "psql-specific features"
 	# see: <https://www.postgresql.org/docs/current/app-psql.html#APP-PSQL-INTERPOLATION>
-	psql --no-psqlrc -v "table_schema=$table_schema" -v "table_name=$table_name" -v "doc_column=$doc_column" -v "file_to_load=$file_to_load" -f "$psql_file"|sed 's/^INSERT /UPSERT /'
+	psql --no-psqlrc --quiet --expanded --tuples-only -v "table_schema=$table_schema" -v "table_name=$table_name" -v "doc_column=$doc_column" -v "file_to_load=$file_to_load" -f "$psql_file" | grep -v ^$
 
 	shift
 done
